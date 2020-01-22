e7f88189801cd8e59d7f722499d5574577f3c9f0
Jacklyn <jih045@ucsd.edu>
2020-01-21 22:51:13 -0800

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by mac air on 2020/1/21.
+//  Copyright © 2020 mac air. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
